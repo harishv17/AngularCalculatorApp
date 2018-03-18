3ce34a7604ba412dcb2f0f9e4981f36ac90312d8
harishv17 <vashishth.harish17@gmail.com>
2018-03-18 20:41:00 +0100

    Update

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ var app = angular.module("myApp", []);
 app.controller("CalculatorCtrl", CalculatorCtrl);
 
 function CalculatorCtrl(){
-    
+    this.resultValue = 0;
+    this.buttonClick = function(button){
+        this.selectedOperation = button;
+    }
+    this.calculate = function(){
+        var number1 = parseFloat(this.input1);
+        var number2 = parseFloat(this.input2);
+    }
 }
