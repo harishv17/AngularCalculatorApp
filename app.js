da9cb349d0b2f77f825f875fd66b9dd78113bfbc
harishv17 <vashishth.harish17@gmail.com>
2018-03-18 20:42:57 +0100

    Updated

--- app.js
+++ app.js
@@ -9,5 +9,17 @@ function CalculatorCtrl(){
     this.calculate = function(){
         var number1 = parseFloat(this.input1);
         var number2 = parseFloat(this.input2);
+        if(this.selectedOperation === '+'){
+            this.resultValue = number1 + number2;
+        }
+        else if(this.selectedOperation === '-'){
+            this.resultValue = number1 - number2;
+        }
+        else if(this.selectedOperation === '*'){
+            this.resultValue = number1 - number2;
+        }
+        else if(this.selectedOperation === '/'){
+            this.resultValue = number1 - number2;
+        }
     }
 }
